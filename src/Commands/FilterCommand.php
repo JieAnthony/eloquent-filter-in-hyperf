03d35d2d9f72a74ca71699b79779f8e2a43f0176
Anthony <407968526@qq.com>
2020-08-06 10:42:51 +0800

    fix command

--- src/Commands/FilterCommand.php
+++ src/Commands/FilterCommand.php
@@ -13,46 +13,16 @@ namespace JieAnthony\EloquentFilter\Commands;
 
 use Hyperf\Command\Annotation\Command;
 use Hyperf\Devtool\Generator\GeneratorCommand;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * @Command
  */
 class FilterCommand extends GeneratorCommand
 {
-    protected $name = 'gen:eloquent-filter';
-
     public function __construct()
     {
-        parent::__construct($this->name);
+        parent::__construct('gen:eloquent-filter');
         $this->setDescription('Create a new model filter class');
-    }
-
-    public function execute(InputInterface $input, OutputInterface $output)
-    {
-        $this->input = $input;
-        $this->output = $output;
-
-        $inputs = $this->getNameInput();
-        $name = $this->qualifyClass($inputs['name']);
-
-        $path = $this->getPath($name);
-
-        if (($input->getOption('force') === false) && $this->alreadyExists($inputs['name'])) {
-            $output->writeln(sprintf('<fg=red>%s</>', $name . ' already exists!'));
-            return false;
-        }
-        $this->makeDirectory($path);
-        file_put_contents($path, $this->buildModelFilterClass($name));
-        $output->writeln(sprintf('<info>%s</info>', $name . ' created successfully.'));
-    }
-
-    protected function buildModelFilterClass($name)
-    {
-        $stub = file_get_contents($this->getStub());
-        return $this->replaceNamespace($stub, $name)->replaceClass($stub, $name);
     }
 
     protected function getStub(): string
@@ -62,30 +32,6 @@ class FilterCommand extends GeneratorCommand
 
     protected function getDefaultNamespace(): string
     {
-        return $this->getConfig()['namespace'] ?? 'App\\ModelFilters';
-    }
-
-    /**
-     * Get the desired class name from the input.
-     *
-     * @return array
-     */
-    protected function getNameInput()
-    {
-        return [
-            'name' => trim($this->input->getArgument('name')),
-        ];
-    }
-
-    /**
-     * Get the console command arguments.
-     *
-     * @return array
-     */
-    protected function getArguments()
-    {
-        return [
-            ['name', InputArgument::REQUIRED, 'The name of the model filter class'],
-        ];
+        return config('eloquentfilter.namespace') ?? 'App\\ModelFilters';
     }
 }
